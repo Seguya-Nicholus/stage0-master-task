3114048064ef707327344a9cfccd9e5fe894bef5
Nicholas Sseguya <seguyanicholus2015@gmail.com>
2023-06-07 14:58:00 +0300

    Conditions[Task 4. Integer divider.]

--- src/main/java/com/epam/conditions/IntegerDivider.java
+++ src/main/java/com/epam/conditions/IntegerDivider.java
@@ -3,6 +3,16 @@ package com.epam.conditions;
 public class IntegerDivider {
 
     public void printCompletelyDivided(int dividend, int divider) {
+        double result = dividend / divider;
+
+        int product = Integer.valueOf((int) (result * divider));
+        if(product == 0) {
+            System.out.println("division by zero");
+        }else if(product == dividend){
+            System.out.println("can be divided completely");
+        }else {
+            System.out.println("cannot be divided completely");
+        }
     }
 
 }
