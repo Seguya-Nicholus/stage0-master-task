360913fa558600e3d8d7776e61f42738247c1aaf
Nicholas Sseguya <seguyanicholus2015@gmail.com>
2023-06-07 16:13:15 +0300

    Loops[Task 3. Power]

--- src/main/java/com/epam/loops/Power.java
+++ src/main/java/com/epam/loops/Power.java
@@ -1,9 +1,15 @@
 package com.epam.loops;
 
+import javax.naming.spi.DirStateFactory.Result;
+
 public class Power {
 
     public void printPower(int numberToPrint, int power) {
-
+        int result = 1;
+        for(int i = 0; i < power; i++){
+            result = (numberToPrint * i);
+        }
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
