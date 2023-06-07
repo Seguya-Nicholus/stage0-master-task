b3d9a40a32c70c0d3c5299b2421fe2809e48d487
Nicholas Sseguya <seguyanicholus2015@gmail.com>
2023-06-07 15:52:45 +0300

    modified

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -4,8 +4,8 @@ public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
         if (number == 0 || number < 0)
-            return 0;
-        return number % 10 + calculateSum(number / 10);
+            System.out.println(0);;
+        System.out.println(number % 10 + calculateSum(number / 10));
     }
 
 }
