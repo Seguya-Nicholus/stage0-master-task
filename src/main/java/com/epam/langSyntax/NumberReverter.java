9cc209621375c4db242d762db5610651ad63d6fc
Nicholas Sseguya <seguyanicholus2015@gmail.com>
2023-06-07 14:34:12 +0300

    Lang Syntax[Task 1. Numbers reverter]

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,13 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        int reversed = 0;
+        while (number != 0) {
+           int remainder = number % 10;
+           reversed = (reversed * 10) + 10;
+           number = number/10;
+        }
+        System.out.println(reversed);
     }
 
 }
