2ad7d65f2cb22cfe10cca551219c2303ce92ac9e
Nicholas Sseguya <seguyanicholus2015@gmail.com>
2023-06-07 14:35:16 +0300

    Lang Syntax[Task 2. Digits sum calculator.]

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,9 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+        if (number == 0 || number < 0)
+            return 0;
+        return number % 10 + calculateSum(number / 10);
     }
 
 }
