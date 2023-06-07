b1d30d81595f1026dd79f48abd355e41de9377f4
Nicholas Sseguya <seguyanicholus2015@gmail.com>
2023-06-07 14:36:01 +0300

    Conditions[Task 1. Season determiner.]

--- src/main/java/com/epam/conditions/SeasonDeterminer.java
+++ src/main/java/com/epam/conditions/SeasonDeterminer.java
@@ -3,6 +3,44 @@ package com.epam.conditions;
 public class SeasonDeterminer {
 
     public void tellTheSeason(int monthNumber) {
+        if (monthNumber == 0) {
+            System.out.println("Wrong month number");
+        }
+
+        if (monthNumber == 12) {
+            System.out.println("Winter");
+        }
+
+        for(int i=1; i<3; i++){
+            if(i==monthNumber){
+                System.out.println("Winter");
+            }
+        }
+
+        for(int i=3; i<6; i++){
+            if(i==monthNumber){
+                System.out.println("Spring");
+            }
+        }
+
+        for(int i=6; i<9; i++){
+            if(i==monthNumber){
+                System.out.println("Summer");
+            }
+        }
+
+        for(int i=6; i<9; i++){
+            if(i==monthNumber){
+                System.out.println("Summer");
+            }
+        }
+
+        for(int i=9; i<12; i++){
+            if(i==monthNumber){
+                System.out.println("Autumn");
+            }
+        }
+        
     }
 
 }
