5228a9165ff91a7467cccb69086edd28282bb299
Nicholas Sseguya <seguyanicholus2015@gmail.com>
2023-06-07 15:23:35 +0300

    Loops[Task 1. Multiplication table]

--- src/main/java/com/epam/loops/MultiplicationTable.java
+++ src/main/java/com/epam/loops/MultiplicationTable.java
@@ -1,8 +1,18 @@
 package com.epam.loops;
 
+import java.util.Scanner;
+
 public class MultiplicationTable {
 
     public void printTable(int numberTableToPrint) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a positive number : ");
+        int number = scanner.nextInt();
+        int i = 0;
+        while (!(i < numberTableToPrint)) {
+            System.out.println(number +" x " + i + " = " + (number*i));
+            i++;
+        }
     }
 
 }
