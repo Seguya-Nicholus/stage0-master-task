ba34fd4ac5b6ef1bb26ef3855cf2938006b54b63
Nicholas Sseguya <seguyanicholus2015@gmail.com>
2023-06-07 15:58:09 +0300

    Loops[Task 4. Half-Pyramid]

--- src/main/java/com/epam/loops/HalfPyramid.java
+++ src/main/java/com/epam/loops/HalfPyramid.java
@@ -3,7 +3,14 @@ package com.epam.loops;
 public class HalfPyramid {
 
     public void printHalfPyramid(int cathetusLength) {
-
+        int i, j;
+        for(i=0; i<cathetusLength; i++)
+        {  
+            for(j=0; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
