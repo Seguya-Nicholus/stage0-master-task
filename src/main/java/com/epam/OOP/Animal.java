94c5a1d9bad0b72697e1dfe17140bdb7d64af3a8
Nicholas Sseguya <seguyanicholus2015@gmail.com>
2023-06-07 14:32:13 +0300

    Task 1

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -1,5 +1,18 @@
 package com.epam.OOP;
 
 public class Animal {
+    protected String color;
+    protected int numberOfPaws;
+    protected boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription(){
+        return "This animal is mostly " + this.color + ". It has " + this.numberOfPaws + " paws and ('a'/'no' -> depends on value of hasFur) fur.";
+    }
 
 }
