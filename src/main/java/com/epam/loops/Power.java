75db19fd7e926e13a6c8c65a0c77af27e9dfcf86
Nicholas Sseguya <seguyanicholus2015@gmail.com>
2023-06-08 21:58:39 +0300

    removed unused import

--- src/main/java/com/epam/loops/Power.java
+++ src/main/java/com/epam/loops/Power.java
@@ -1,19 +1,16 @@
 package com.epam.loops;
 
-import javax.naming.spi.DirStateFactory.Result;
-
 public class Power {
 
-    public void printPower(int numberToPrint, int power) {
-        long result = 1;
-        for(int i = 0; i < power; i++){
-            result =  result*numberToPrint;
-        }
-        System.out.println(result);
+  public void printPower(int numberToPrint, int power) {
+    long result = 1;
+    for (int i = 0; i < power; i++) {
+      result = result * numberToPrint;
     }
+    System.out.println(result);
+  }
 
-    public static void main(String[] args) {
-        new Power().printPower(10, 3);
-    }
-
+  public static void main(String[] args) {
+    new Power().printPower(10, 3);
+  }
 }
